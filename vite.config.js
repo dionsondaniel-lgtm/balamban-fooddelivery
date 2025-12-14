9ff7c84ddd37fca0dcb9bfbae429fe77b69b4391
Daniel Dionson <dionsondaniel@gmail.com>
2025-12-14 13:39:04 +0800

    Dec14 Update5

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensure root path for Vercel
+  build: {
+    outDir: 'dist', // default folder; optional
+  },
 });
